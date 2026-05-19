63fb62ff22cefdaea4be3aa6a7e31c410ea0a1e1
DESKTOP-KON806E\JoaoRamos <joaoramos00@gmail.com>
2026-05-19 18:55:28 -0300

    feat: wire test level to Zael

--- scenes/test_level.gd
+++ scenes/test_level.gd
@@ -1,12 +1,13 @@
 extends Node2D
 
-@onready var character: CharacterBase = $CharacterBase
-@onready var camera: Camera2D = $Camera2D
+@onready var character: Zael = $Zael
 
 func _ready() -> void:
     GameManager.reset()
     GameManager.set_active_character("zael")
     StageManager.spawn_position = character.global_position
+    queue_redraw()
 
-func _process(_delta: float) -> void:
-    camera.global_position = character.global_position
+func _draw() -> void:
+    draw_rect(Rect2(-500, 500, 3000, 40), Color(0.3, 0.3, 0.3))
+    draw_rect(Rect2(600, 390, 200, 20), Color(0.4, 0.4, 0.4))
