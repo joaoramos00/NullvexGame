f6e02e3f22ac2fee69adb3b034ce6da5d360c5da
DESKTOP-KON806E\JoaoRamos <joaoramos00@gmail.com>
2026-06-11 11:51:48 -0300

    feat: animate stage 02 looping enemies with sheets

--- characters/enemies/stage_02/enemy_ice_grunt.gd
+++ characters/enemies/stage_02/enemy_ice_grunt.gd
@@ -1,6 +1,9 @@
 extends EnemyBase
 class_name EnemyIceGrunt
 
+const RUN_FRAMES := 6
+const RUN_FPS := 10.0
+
 func _init() -> void:
 	max_hp = 10
 	contact_damage = 8
@@ -9,3 +12,7 @@ func _ready() -> void:
 	max_hp = 10
 	contact_damage = 8
 	super._ready()
+
+func _physics_process(delta: float) -> void:
+	super._physics_process(delta)
+	_advance_sprite_frame_loop(delta, RUN_FRAMES, RUN_FPS)
