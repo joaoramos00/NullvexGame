ebd9413c21606adf11e5456773d24de6b9fc5ea2
DESKTOP-KON806E\JoaoRamos <joaoramos00@gmail.com>
2026-06-20 08:35:16 -0300

    feat(stage01): enemy_magma_grunt (patrulha simples)

---
+++ characters/enemies/stage_01/enemy_magma_grunt.gd
@@ -0,0 +1,18 @@
+extends EnemyBase
+class_name EnemyMagmaGrunt
+
+const IDLE_FRAMES := 4
+const IDLE_FPS := 6.0
+
+func _init() -> void:
+	max_hp = 10
+	contact_damage = 8
+
+func _ready() -> void:
+	max_hp = 10
+	contact_damage = 8
+	super._ready()
+
+func _physics_process(delta: float) -> void:
+	super._physics_process(delta)
+	_advance_sprite_frame_loop(delta, IDLE_FRAMES, IDLE_FPS)
