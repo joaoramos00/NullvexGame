6606c12c643c10da81b3bf4fc666ea5a081a27ab
DESKTOP-KON806E\JoaoRamos <joaoramos00@gmail.com>
2026-07-18 10:28:38 -0300

    feat(stage08): inimigos voadores/suspensos (gem_wasp, stone_glider, ore_orbiter)

---
+++ characters/enemies/stage_08/enemy_gem_wasp.gd
@@ -0,0 +1,14 @@
+extends "res://characters/enemies/enemy_flyer.gd"
+class_name EnemyGemWasp
+
+# Vespa-gema patrulheira: mergulhador padrão, dive rápido quando detecta o player.
+func _ready() -> void:
+	super._ready()
+	max_hp = 6
+	contact_damage = 6
+	current_hp = max_hp
+	detect_radius = 260.0
+	dive_speed = 370.0
+	retreat_speed = 190.0
+	patrol_range = 200.0
+	bob_amplitude = 10.0
