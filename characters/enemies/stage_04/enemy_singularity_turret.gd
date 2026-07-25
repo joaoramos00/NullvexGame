86f750c1bf0826140ca42b9fe3e7b23af90b5326
DESKTOP-KON806E\JoaoRamos <joaoramos00@gmail.com>
2026-07-24 23:47:06 -0300

    feat: rebalance stage 04 enemies and Gravitus

--- characters/enemies/stage_04/enemy_singularity_turret.gd
+++ characters/enemies/stage_04/enemy_singularity_turret.gd
@@ -17,12 +17,12 @@ const BOLT_DAMAGE := 8
 var _shoot_timer := 0.4
 
 func _init() -> void:
-	max_hp = 18
-	contact_damage = 9
+	max_hp = 4
+	contact_damage = 1
 
 func _ready() -> void:
-	max_hp = 18
-	contact_damage = 9
+	max_hp = 4
+	contact_damage = 1
 	super._ready()
 	_direction = face_dir
 	_sprite.flip_h = _direction < 0.0
