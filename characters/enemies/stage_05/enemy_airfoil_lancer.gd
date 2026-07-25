ef0898a31dd80ae5515ce7d14046158865639acd
DESKTOP-KON806E\JoaoRamos <joaoramos00@gmail.com>
2026-07-24 23:50:35 -0300

    feat: rebalance stage 05 enemies, Galerix HP only (attacks pending)

--- characters/enemies/stage_05/enemy_airfoil_lancer.gd
+++ characters/enemies/stage_05/enemy_airfoil_lancer.gd
@@ -19,12 +19,12 @@ var _shot_frame_timer := 0.0
 var _shot_released := false
 
 func _init() -> void:
-	max_hp = 11
-	contact_damage = 6
+	max_hp = 3
+	contact_damage = 1
 
 func _ready() -> void:
-	max_hp = 11
-	contact_damage = 6
+	max_hp = 3
+	contact_damage = 1
 	super._ready()
 
 func _physics_process(delta: float) -> void:
